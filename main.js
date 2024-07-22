92302b1ebb5490e59ef44134d82eedb6e53eaaf1
Preetii1729 <nirmadan.preeti2004@gmail.com>
2024-07-22 22:22:22 +0530

    Accessed elements and declared some variables

---
+++ main.js
@@ -0,0 +1,10 @@
+var inp_gen=document.getElementById("arr_generate");
+var inp_aspeed=document.getElementById("arr_speed");
+var inp_as=document.getElementById('arr_size');
+var array_size=inp_as.value;
+
+var butts_algos=document.querySelectorAll(".sorts button");
+var div_sizes=[];
+var divs=[];
+var margin_size;
+
