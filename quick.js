82a7c2838467f7adc195a10bf18e4d764cd31b48
Preetii1729 <nirmadan.preeti2004@gmail.com>
2024-07-28 18:58:03 +0530

    Quick Sort Animation finalised

--- quick.js
+++ quick.js
@@ -1,34 +1,71 @@
 function QSort() {
     c_delay=0;
-    console.log("Hello");
+    console.log("Bye2");
     quick(0, array_size-1);
 }
 
 function partition(lb,ub){
     var pivot=lb;
     var i=lb,j=ub;
-    while(i<=j){
-        while(div_sizes[pivot]>=div_sizes[i] && i<=ub)
+    div_update(divs[pivot],div_sizes[pivot]*2,"#BCB57B"); //sage- brown yellow
+
+    while(i<j){
+        while(div_sizes[pivot]>=div_sizes[i] && i<=ub){
+            if(i==lb){
+                i++;
+                continue;
+            }
+            div_update(divs[i],div_sizes[i]*2,"#EE964B");       //Sandy brown
+            div_update(divs[i],div_sizes[i]*2,"#FFC60B");       //og yellow         
             i++;
-        while(div_sizes[pivot]<div_sizes[j] && j>=lb)
+        }
+        
+        if(i<=ub){
+            div_update(divs[i],div_sizes[i]*2,"#EE964B"); 
+            div_update(divs[i],div_sizes[i]*2,"#49697F");     //gray-blue
+        }
+        
+        while(div_sizes[pivot]<div_sizes[j] && j>=lb){
+            div_update(divs[j],div_sizes[j]*2,"#EE964B");   //Sandy brown
+            div_update(divs[j],div_sizes[j]*2,"#FFC60B");     //og yellow
             j--;
+        }
+        div_update(divs[j],div_sizes[j]*2,"#EE964B");
+        div_update(divs[j],div_sizes[j]*2,"#49697F");       //gray-blue
+
         if(i<j){
             var temp=div_sizes[i];
             div_sizes[i]=div_sizes[j];
             div_sizes[j]=temp;
+
+            div_update(divs[i],div_sizes[i]*2,"#49697F");     //gray-blue 
+            div_update(divs[j],div_sizes[j]*2,"#49697F");     //gray-blue
+           
+            div_update(divs[j],div_sizes[j]*2,"#FFC60B");     //og yellow
+            div_update(divs[i],div_sizes[i]*2,"#FFC60B");     //og yellow
         }
+
     }
+
+    div_update(divs[j],div_sizes[j]*2,"#49697F");     //gray-blue
+    div_update(divs[lb],div_sizes[lb]*2,"#49697F");     //gray-blue    
+    
     var temp=div_sizes[j];
     div_sizes[j]=div_sizes[lb];
     div_sizes[lb]=temp;
+
+    div_update(divs[lb],div_sizes[lb]*2,"#FFC60B");     //og yellow    
+    div_update(divs[j],div_sizes[j]*2,"#F95738");     //tomato
+
     return j;
 }
 
 
 function quick(lb, ub){
-    if(lb<ub){
+    if(lb<=ub){
         var loc=partition(lb,ub);
         quick(lb,loc-1);
         quick(loc+1,ub);
+        div_update(divs[loc],div_sizes[loc]*2,"#F95738");     //tomato
     }
 }
