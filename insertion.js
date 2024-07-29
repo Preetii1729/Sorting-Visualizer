8bde50ec3de2d11a8f0ebcaae70a9d6d7cb0681d
Preetii1729 <nirmadan.preeti2004@gmail.com>
2024-07-29 22:30:13 +0530

    Insertion complexity updated

--- insertion.js
+++ insertion.js
@@ -1,6 +1,14 @@
 function ISort(){
     c_delay=0;
 
+    bestt.innerText=" O(n)";
+    avgt.innerHTML=" O(n^2)";
+    worstt.innerHTML=" O(n^2)";
+
+    bests.innerHTML=" O(1)";
+    avgs.innerHTML=" O(1)";
+    worsts.innerHTML=" O(1)";
+
     insertion();
 }
 
