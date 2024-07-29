6373e55dc1b217a58809cb914628075b7736e230
Preetii1729 <nirmadan.preeti2004@gmail.com>
2024-07-29 22:56:15 +0530

    BubbleD function added

--- bubble_d.js
+++ bubble_d.js
@@ -9,5 +9,17 @@ function BSortd(){
     avgs.innerHTML=" O(1)";
     worsts.innerHTML=" O(1)";
 
-    
+    bubbleD();
+}
+
+function bubbleD(){
+    for (var i = 0; i < array_size - 1; i++) {
+        for (var j = 0; j < array_size - i - 1; j++) {
+            if (div_sizes[j] < div_sizes[j + 1]) {      // Changed comparison operator
+                var temp = div_sizes[j];
+                div_sizes[j] = div_sizes[j + 1];
+                div_sizes[j + 1] = temp;
+            }
+        }
+    }
 }
