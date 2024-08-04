b8f61b033e20ddc9f6fbc4a8a099b2a7b4a1bbe4
Preetii1729 <nirmadan.preeti2004@gmail.com>
2024-08-04 20:43:02 +0530

    Insertion Animation corrected

--- insertion.js
+++ insertion.js
@@ -15,7 +15,8 @@ function ISort(){
 function insertion() {
     var n=a_size;
     for(var i=1; i<n ;i++){
-        div_update(divs[i],div_sizes[i]*2,"#BCB57B"); //sage- brown yellow
+
+        div_update(divs[i],div_sizes[i]*2,"#BCB57B");       //sage- brown yellow
 
         j=i-1;
         
@@ -26,35 +27,25 @@ function insertion() {
             div_update(divs[j+1],div_sizes[j+1]*2, "#EE964B"); //sandy brown
             
             div_sizes[j+1]=div_sizes[j];
-            
-            div_update(divs[j],div_sizes[j]*2, "#EE964B"); //sandy brown
-            div_update(divs[j+1],div_sizes[j+1]*2, "#EE964B"); //sandy brown
 
-            div_update(divs[j],div_sizes[j]*2, "#EFC11A");  //og yellow
-            
-            if(j==(i-1))
-            {
-                div_update(divs[j+1],div_sizes[j+1]*2,"#49697F");     //gray blue
-            }
-            else
-            {
-                div_update(divs[j+1],div_sizes[j+1]*2,"#EFC11A");      //og yellow
-            }
-
-
+            div_update(divs[j+1],div_sizes[j+1]*2, "#EE964B"); //sandy brown            
             j--;
         }
 
         div_sizes[j+1]=temp;
+        if(j!=i-1){
+            div_update(divs[j+1],div_sizes[j+1]*2, "#49697F");  //gray blue
+        }
+        else{
+            div_update(divs[j],div_sizes[j]*2, "#49697F");  //gray blue
+        }
+        
         
         for(var t=0;t<i;t++)
         {
             div_update(divs[t],div_sizes[t]*2,"#F95738");      //tomato
         }
 
-        div_update(divs[i-1],div_sizes[i-1]*2, "#F95738"); //tomato
-
-        // div_update(divs[j],div_sizes[j]*2, "#F95738");//Color update - tomato
     }
     div_update(divs[n-1],div_sizes[n-1]*2, "#F95738"); //tomato
     
